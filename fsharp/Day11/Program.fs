8f892a11033d6f29c674feee16b54eae4d112245
Juuz <6596629+Juuxel@users.noreply.github.com>
2024-12-11 16:18:26 +0200

    Day 11 (F#): Clean up

--- fsharp/Day11/Program.fs
+++ fsharp/Day11/Program.fs
@@ -1,40 +1,29 @@
-﻿open System.Collections.Generic
+open System.Collections.Generic
 open System.Numerics
-
-type CacheKey = { Depth: int
-                  Value: BigInteger }
 
 let numberOfDigits (num: BigInteger) =
     1 + (BigInteger.Log10 num |> int)
 
-let getOrPut (dict: Dictionary<'a, 'b>) key value =
-    if dict.ContainsKey key then
-        dict[key]
+let rec blink (cache: Dictionary<int * BigInteger, int64>) depth num =
+    let cacheKey = (depth, num)
+    if cache.ContainsKey cacheKey then
+        cache[cacheKey]
     else
-        let computed = value()
-        dict[key] <- computed
-        computed
-
-let rec blink (cache: Dictionary<CacheKey, int64>) depth (num: BigInteger) =
-    let cacheKey =
-        { Depth = depth
-          Value = num }
-    getOrPut cache cacheKey (fun () ->
-        if cache.ContainsKey cacheKey then
-            cache[cacheKey]
-        else if depth <= 0 then
-            1L
-        elif num = BigInteger.Zero then
-            blink cache (depth - 1) BigInteger.One
-        else
-            let d = numberOfDigits num
-            if d % 2 = 0 then
-                let div = BigInteger.Pow(new BigInteger(10), d / 2)
-                let (l, r) = BigInteger.DivRem(num, div)
-                (blink cache (depth - 1) l) + (blink cache (depth - 1) r)
+        let result =
+            if depth <= 0 then
+                1L
+            elif num = BigInteger.Zero then
+                blink cache (depth - 1) BigInteger.One
             else
-                blink cache (depth - 1) (num * BigInteger(2024))
-    )
+                let d = numberOfDigits num
+                if d % 2 = 0 then
+                    let div = BigInteger.Pow(new BigInteger(10), d / 2)
+                    let (l, r) = BigInteger.DivRem(num, div)
+                    (blink cache (depth - 1) l) + (blink cache (depth - 1) r)
+                else
+                    blink cache (depth - 1) (num * BigInteger(2024))
+        cache[cacheKey] <- result
+        result
 
 let cache = Dictionary()
 let input =
